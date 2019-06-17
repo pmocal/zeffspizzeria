c20daeab9c6cf3a2bbd9fe770a03178fe81250b1
Parthiv Mohan <parthiv.mohan@gmail.com>
2019-06-17 14:37:25 -0400

    removed duplicate of word menu

--- src/tab2.js
+++ src/tab2.js
@@ -1,6 +1,5 @@
 function tab2() {
-	document.getElementById("tabcontent").innerHTML = "<h2><span>&nbsp;Menu&nbsp;</span></h2>";
-	document.getElementById("tabcontent").innerHTML += "<h3>Food</h3>";
+	document.getElementById("tabcontent").innerHTML = "<h3>Food</h3>";
 	document.getElementById("tabcontent").innerHTML += "<p>Pizza</p>";
 	document.getElementById("tabcontent").innerHTML += "<p>Garlic Knots</p>";
 	document.getElementById("tabcontent").innerHTML += "<p>Salad</p>";
